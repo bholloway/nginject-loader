51ec50c9ac75884ef01d1a9e015f503e4aaefeea
benholloway <ben.holloway@3plearning.com>
2015-09-30 09:24:39 +1000

    absorbed/duplicated browserify-nginject dependency in order to fix bugs

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
 
-var loaderUtils = require('loader-utils'),
-    getUpdater  = require('browserify-nginject/lib/get-updater'),
-    processSync = require('browserify-esprima-tools').processSync,
-    convert     = require('convert-source-map');
+var loaderUtils    = require('loader-utils'),
+    codegen        = require('escodegen'),
+    esprima        = require('esprima'),
+    sourcemapToAst = require('sourcemap-to-ast');
+
+var esprimaTools = require('./lib/esprima-tools'),
+    updater      = require('./lib/updater');
 
 /**
  * Webpack loader where explicit @ngInject comment creates pre-minification $inject property.
@@ -14,21 +17,47 @@ var loaderUtils = require('loader-utils'),
 module.exports = function loader(content, sourceMap) {
   /* jshint validthis:true */
 
+  // loader result is cacheable
+  this.cacheable();
+
   // path of the file being processed
   var filename = this.resourcePath,
       options  = loaderUtils.parseQuery(this.query);
 
-  // loader result is cacheable
-  this.cacheable();
+  // parse code to AST using esprima
+  var ast = esprima.parse(content, {
+    loc    : true,
+    comment: true,
+    source : filename
+  });
 
-  // process
-  try {
-    var output    = processSync(filename, content, getUpdater(this.emitError)),
-        converter = convert.fromSource(output),
-        source    = output.replace(/\/(?:\/|\*)[@#]\s+sourceMappingURL[^]*$/, ''),
-        map       = converter && converter.toObject();
-    this.callback(null, source, map);
-  } catch (exception) {
-    this.emitError(exception);
+  // sort nodes before changing the source-map
+  var sorted = esprimaTools.orderNodes(ast);
+
+  // associate comments with nodes they annotate before changing the sort map
+  esprimaTools.associateComments(ast, sorted);
+
+  // make sure the AST has the data from the original source map
+  var useSourceMap = sourceMap && options.sourceMap;
+  if (useSourceMap) {
+    sourcemapToAst(ast, sourceMap);
+  }
+
+  // update the AST
+  var updated = updater(this.emitError)(ast, options);
+
+  // generate compressed code from the AST
+  var output = codegen.generate(updated, {
+        sourceMap        : useSourceMap,
+        sourceMapWithCode: useSourceMap,
+        format           : options.format || {}
+      }),
+      outMap = JSON.parse(output.map.toString());
+
+  // complete
+  if (useSourceMap) {
+    this.callback(null, output.code, outMap);
+  } else {
+    return output;
   }
 };
