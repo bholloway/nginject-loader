97a134fcdb0bc9fc9cc01f9603d13a354bf7f139
benholloway <ben.holloway@3plearning.com>
2015-11-03 16:22:31 +1100

    migration-centric functionality based on nginject-migrate

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 'use strict';
 
-var loaderUtils    = require('loader-utils'),
-    codegen        = require('escodegen'),
-    esprima        = require('esprima'),
-    sourcemapToAst = require('sourcemap-to-ast');
-
-var esprimaTools = require('./lib/esprima-tools'),
-    updater      = require('./lib/updater');
+var loaderUtils = require('loader-utils'),
+    migrate     = require('nginject-migrate');
 
 /**
  * Webpack loader where explicit @ngInject comment creates pre-minification $inject property.
@@ -22,42 +17,20 @@ module.exports = function loader(content, sourceMap) {
 
   // path of the file being processed
   var filename = this.resourcePath,
-      options  = loaderUtils.parseQuery(this.query);
-
-  // parse code to AST using esprima
-  var ast = esprima.parse(content, {
-    loc    : true,
-    comment: true,
-    source : filename
-  });
-
-  // sort nodes before changing the source-map
-  var sorted = esprimaTools.orderNodes(ast);
-
-  // associate comments with nodes they annotate before changing the sort map
-  esprimaTools.associateComments(ast, sorted);
+      options  = loaderUtils.parseQuery(this.query),
+      useMap   = loader.sourceMap || options.sourceMap;
 
   // make sure the AST has the data from the original source map
-  var useSourceMap = sourceMap && options.sourceMap;
-  if (useSourceMap) {
-    sourcemapToAst(ast, sourceMap);
-  }
-
-  // update the AST
-  var updated = updater(this.emitError)(ast, options);
-
-  // generate compressed code from the AST
-  var output = codegen.generate(updated, {
-        sourceMap        : useSourceMap,
-        sourceMapWithCode: useSourceMap,
-        format           : options.format || {}
-      });
+  var pending = migrate.processSync(content, {
+    filename : filename,
+    sourceMap: useMap && (sourceMap || true),
+    quoteChar: options.quoteChar
+  });
 
   // complete
-  if (useSourceMap) {
-    var outputMapObj = JSON.parse(output.map.toString());
-    this.callback(null, output.code, outputMapObj);
+  if (useMap) {
+    this.callback(null, pending.content, pending.sourceMap);
   } else {
-    return output;
+    return pending.content;
   }
 };
